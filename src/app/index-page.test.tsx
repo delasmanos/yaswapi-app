95b24297b5093453e06bdef4a363e3931bbd9876
delasmanos <delasmanosk@proton.me>
2026-02-08 01:42:38 +0100

    test: add a11y test for home page

---
+++ src/app/index-page.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Page from "./page";
+
+describe("IndexPage Accessibility", () => {
+  it("should render the page title", () => {
+    render(<Page />);
+    const titleElement = screen.getByRole("heading", {
+      level: 1,
+      name: "Yet another SWAPI App",
+    });
+    expect(titleElement).toBeInTheDocument();
+  });
+});
